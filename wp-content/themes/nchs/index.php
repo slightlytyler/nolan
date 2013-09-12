d9c92b08a86cd1a27edc2bef16b4d920e471d87e
Daniel Pérez <dapanas@gmail.com>
2013-09-12 00:40:32 -0500

    Adds Latest News widget. Removes hardcoded "Latest news" from index.php.

--- wp-content/themes/nchs/index.php
+++ wp-content/themes/nchs/index.php
@@ -11,32 +11,6 @@ get_header(); ?>
       </div>
       <div class='info_section'>
         <?php get_sidebar("homepage-top")?>
-        <?php
-        $args = array( 'post_type' => 'post', 'posts_per_page' => 3, 'tag' => 'homepage' );
-        $loop = new WP_Query( $args );
-        ?>
-
-        <?php if ($loop->have_posts()): ?>
-        <div class='info_news'>
-            <div class='info_news_title'>Latest News</div>
-          <?php
-          while ( $loop->have_posts() ) :
-            $loop->the_post();
-          ?>
-          <div class='media'>
-            <a class='pull-left' href='#'>
-              <?php the_post_thumbnail('nchs-index-latest-news-thumb'); ?>
-            </a>
-            <div class='media-body'>
-              <h4 class='media-heading'>
-                <a href='<?php the_permalink(); ?>'><?php the_title(); ?></a>
-              </h4>
-              <div class='news_date'><?php the_time('D M j'); ?></div>
-            </div>
-          </div>
-          <?php endwhile; ?>
-        </div>
-        <?php endif; ?>
         <div class='clear'></div>
       </div>
     </div>
