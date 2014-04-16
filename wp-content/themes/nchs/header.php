5d0a1371b3d529ff29e84ab3732fef81a4821acd
Taylor Young <tdy721@gmail.com>
2014-04-16 17:43:01 -0500

    <head> improvements from bootstrap

--- wp-content/themes/nchs/header.php
+++ wp-content/themes/nchs/header.php
@@ -9,16 +9,34 @@
 * @since Vype 1.0
 */
 ?>
-<!DOCTYPE HTML>
-<html <?php language_attributes(); ?>>
+<!doctype html>  
+
+<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
+<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
   <head>
-    <meta charset='utf-8'>
-    <title><?php wp_title(); ?></title>
-    <meta content='' name='description'>
-    <meta content='width=device-width' name='viewport'>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php wp_title( '|', true, 'right' ); ?></title> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- media-queries.js (fallback) -->
+    <!--[if lt IE 9]>
+      <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>     
+    <![endif]-->
+
+    <!-- html5.js -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
     <?php wp_head(); ?>
+
+    <!-- @todo load these properly... -->
 
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css"> -->
