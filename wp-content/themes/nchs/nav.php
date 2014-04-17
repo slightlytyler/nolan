0f7730e91a0143b754f2f039a9c88cc1d61915b1
Taylor Young <tdy721@gmail.com>
2014-04-17 18:27:56 -0500

    fixed merge miss

--- wp-content/themes/nchs/nav.php
+++ wp-content/themes/nchs/nav.php
@@ -1,13 +1,10 @@
 <div class="navbar navbar-main navbar-inverse hidden-xs"><!-- navbar-fixed-top -->
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-    <?php nhcs_get_nav( 'nchs-nav-menu' ); ?>
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
   </div>
   <?php nhcs_get_nav( 'nchs-nav-menu' ); ?>
 </div>
