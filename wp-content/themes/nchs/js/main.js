3ae1072117894296152beef78efe05f698786aca
Taylor Young <tdy721@gmail.com>
2014-04-18 22:26:43 -0500

    javascript fix

--- wp-content/themes/nchs/js/main.js
+++ wp-content/themes/nchs/js/main.js
@@ -1,52 +1,50 @@
 if (typeof jQuery !== 'undefined') {
   jQuery(function($) {
-		$(document).ready(function(){
-            // ****************************************** PLACEHOLDER       
-            function placeholder() { // placeholder for inputs textarea
-              //placeholder for form    
-              $('input, textarea').focus(function() {
-                if($(this).attr('placeholder') == $(this).val()){
-                    $(this).val('');  
-                    $(this).data('placeholder', $(this).attr('placeholder'));
-                    $(this).attr('placeholder','');  
-                }
-              });
-              $('input, textarea').blur(function() {
-                if ($(this).val() == '') {
-                  $(this).val($(this).data('placeholder'));
-                  $(this).attr('placeholder',$(this).data('placeholder'));  
-                }
-              });     
-            };
-            placeholder();
-          	$('.flexslider').flexslider({ // Slider - http://flexslider.woothemes.com/
-              animation: "slide",
-              controlNav: true,
-              manualControls: ".control-nav button"
-            });
-            $('.coach_slider').flexslider({
-              animation: "slide",
-              controlNav: false,
-              itemWidth: 187,
-              animationLoop: true,
-              slideshow: false,
-              move: 1
-            });
-            $('.team_box').flexslider({ // Slider - http://flexslider.woothemes.com/
-              animation: "slide",
-              controlNav: false,
-              direction: "vertical"
-            });
-            $(document).on('click', '.main_menu_btn', function(){
-              $('.main_menu').toggle();
-            })
-						$(window).load(function() {
-							if($(".right_sidebar").height() > $(".page_container").height()){
-							$(".page_container").height(parseInt($(".right_sidebar").height())+100);
-						}
-						    
-						});
-						
+    $(document).ready(function(){
+      // ****************************************** PLACEHOLDER       
+      function placeholder() { // placeholder for inputs textarea
+        //placeholder for form    
+        $('input, textarea').focus(function() {
+          if($(this).attr('placeholder') == $(this).val()){
+              $(this).val('');  
+              $(this).data('placeholder', $(this).attr('placeholder'));
+              $(this).attr('placeholder','');  
+          }
         });
-	});
+        $('input, textarea').blur(function() {
+          if ($(this).val() == '') {
+            $(this).val($(this).data('placeholder'));
+            $(this).attr('placeholder',$(this).data('placeholder'));  
+          }
+        });
+      };
+      placeholder();
+      $('.flexslider').flexslider({ // Slider - http://flexslider.woothemes.com/
+        animation: "slide",
+        controlNav: true,
+        manualControls: ".control-nav button"
+      });
+      $('.coach_slider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        itemWidth: 187,
+        animationLoop: true,
+        slideshow: false,
+        move: 1
+      });
+      $('.team_box').flexslider({ // Slider - http://flexslider.woothemes.com/
+        animation: "slide",
+        controlNav: false,
+        direction: "vertical"
+      });
+      $(document).on('click', '.main_menu_btn', function(){
+        $('.main_menu').toggle();
+      })
+      $(window).load(function() {
+        if($(".right_sidebar").height() > $(".page_container").height()){
+        $(".page_container").height(parseInt($(".right_sidebar").height())+100);
+      }
+      });
+    });
+  });
 };
