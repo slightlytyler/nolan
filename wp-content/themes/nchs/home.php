b25483a6fe766d4c3af9bc1c5557dc05e41146ff
Taylor Young <tdy721@gmail.com>
2014-04-19 22:49:10 -0500

    Homepage Template + Fluid Ribbon Banner Tail effect

--- wp-content/themes/nchs/home.php
+++ wp-content/themes/nchs/home.php
@@ -1,10 +1,34 @@
 <?php
 get_header();
-echo 
-"<div class='info_title'>
+?>
+<div class='info_title'>
   <h3>In the Marianist tradition since 1961</h3>
-</div>";
-get_sidebar("homepage-top");
+</div>
+<div class="col-md-6">
+  <h2>Latest News</h2>
+<?php 
+while ( have_posts() ) : the_post();
+  the_title('<a href="'.get_permalink().'"><h3>','</h3></a>');
+  the_date();
+  the_author();
+  the_excerpt();
+endwhile;
+?>
+</div>
+<div class="col-md-6">
+  <h2>Video Here</h2>
+</div>
+<div class="col-md-6 banner">
+  <div class="banner-inner">
+    <h3>Banner</h3>
+    <p>Some Content</p>
+    <div class="clearfix"></div>
+  </div>
+  <div class="banner-left"></div><div class="banner-right"></div>
+</div>
+<div class="clearfix"></div>
+<?php
+// get_sidebar("homepage-top");
 get_sidebar('homepage-right');
 get_sidebar('homepage-middle');
 get_sidebar('homepage-bottom');
--- wp-content/themes/nchs/js/main.js
+++ wp-content/themes/nchs/js/main.js
@@ -1,3 +1,34 @@
+function tail_resize() {
+  var banner_tail = ( ( $('.banner').outerWidth() - 30 ) /2 );
+  $('.banner-right').css({ 'width': banner_tail, 'border-left-width': banner_tail });
+  $('.banner-left').css({ 'width': banner_tail, 'border-right-width': banner_tail });
+}
+(function($,sr){
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+})(jQuery,'smartresize');
+
 if (typeof jQuery !== 'undefined') {
   jQuery(function($) {
     $(document).ready(function(){
@@ -76,6 +107,13 @@ if (typeof jQuery !== 'undefined') {
           $('.col-sm-8').append( $table );
         });
       }
+      // Banner Tail Resizing
+      // @todo Just hide it while resize is happening.
+      // @idea Animate out?
+      $(window).smartresize(function(){
+        tail_resize();
+      });
+      tail_resize();
     });
   });
 };
