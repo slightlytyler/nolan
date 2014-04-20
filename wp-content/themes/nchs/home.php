ba176e67cad6bc9f139fbe97680371f84162e5d3
Taylor Young <tdy721@gmail.com>
2014-04-20 00:32:15 -0500

    Homepage Stripe

--- wp-content/themes/nchs/home.php
+++ wp-content/themes/nchs/home.php
@@ -18,19 +18,40 @@ endwhile;
 <div class="col-md-6">
   <h2>Video Here</h2>
 </div>
-<div class="col-md-6 banner">
-  <div class="banner-inner">
-    <h3>Banner</h3>
-    <p>Some Content</p>
-    <div class="clearfix"></div>
+<div class="clearfix"></div>
+<div class="col-md-12 nopad home-stripe">
+  <div class="col-md-8 mission">
+    <h2>NHCS Mission</h2>
+    <p>Yeah, yeah what are you wearing, Dave. I can't play. Pretty Mediocre photographic fakery, they cut off your brother's hair. I know what you're gonna say, son, and you're right, you're right, But Biff just happens to be my supervisor, and I'm afraid I'm not very good at confrontations. Hey man, the dance is over. Unless you know someone else who could play the guitar.</p>
+    <p><button class="btn btn-info">Read More</button></p>
   </div>
-  <div class="banner-left"></div><div class="banner-right"></div>
+  <div class="col-md-4 banner">
+    <div class="banner-inner">
+      <h3>Banner</h3>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <h3>Banner</h3>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <h3>Banner</h3>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <p>Some Content</p>
+      <div class="clearfix"></div>
+    </div>
+    <div class="banner-left"></div><div class="banner-right"></div>
+  </div>
 </div>
 <div class="clearfix"></div>
 <?php
 // get_sidebar("homepage-top");
-get_sidebar('homepage-right');
-get_sidebar('homepage-middle');
+// get_sidebar('homepage-right');
+// get_sidebar('homepage-middle');
 get_sidebar('homepage-bottom');
 get_footer();
 ?>
