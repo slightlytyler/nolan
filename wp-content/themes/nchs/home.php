0625e60928e0a2fb9af0df5bc12770bcd4fe8e3b
Taylor Young <tdy721@gmail.com>
2014-04-21 15:31:56 -0500

    Homepage - stack video on top of news for mobile view

--- wp-content/themes/nchs/home.php
+++ wp-content/themes/nchs/home.php
@@ -4,25 +4,24 @@ get_header();
 <div class='info_title'>
   <h3>In the Marianist tradition since 1961</h3>
 </div>
-
 <div class="col-sm-12 home-top-wrap">
-<div class="col-sm-5 col-md-4 latest">
-  <h2>Latest</h2>
-<?php 
-while ( have_posts() ) : the_post();
-  the_title('<a href="'.get_permalink().'"><h3>','</h3></a>');
-  the_date();
-  echo ' | ';
-  the_author();
-  // the_excerpt();
-endwhile;
-next_posts_link();
-// echo '<a href="/news">More News &raquo;</a>'
-?>
-</div>
-<div class="col-sm-7 col-md-8 video">
-  <h2>Video Here</h2>
-</div>
+  <div class="col-sm-7 col-md-8 col-sm-push-5 col-md-push-4 video">
+    <h2>Video Here</h2>
+  </div>
+  <div class="col-sm-5 col-md-4 col-sm-pull-7 col-md-pull-8 latest">
+    <h2>Latest</h2>
+  <?php 
+  while ( have_posts() ) : the_post();
+    the_title('<a href="'.get_permalink().'"><h3>','</h3></a>');
+    the_date();
+    echo ' | ';
+    the_author();
+    // the_excerpt();
+  endwhile;
+  next_posts_link();
+  // echo '<a href="/news">More News &raquo;</a>'
+  ?>
+  </div>
 </div>
 <div class="clearfix"></div>
 <div class="col-sm-12 nopad home-stripe">
