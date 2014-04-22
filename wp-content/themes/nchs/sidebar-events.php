1458d40523818576c14e72672c24e6a61b574ddd
Taylor Young <tdy721@gmail.com>
2014-04-22 01:02:21 -0500

    fix

--- wp-content/themes/nchs/sidebar-events.php
+++ wp-content/themes/nchs/sidebar-events.php
@@ -1,7 +1,7 @@
 <div class='col-sm-5 col-md-4'>
   <div class='right_sidebar'>
     <ul class='widgets'>
-      <?php if ( !dynamic_sidebar( 'student-sidebar' ) ) {} ?>
+      <?php if ( !dynamic_sidebar( 'events-sidebar' ) ) {} ?>
     </ul>
   </div>
 </div>
